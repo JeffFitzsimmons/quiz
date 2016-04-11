7f12b510e1bf30c23b986e287927720a09daa7c7
Jeff Fitzsimmons <jeff.fitzsimmons@outlook.com>
2016-04-11 10:09:29 -0600

    Oops
    Changes that should have been included in initial.

--- js/master.js
+++ js/master.js
@@ -1,15 +1,5 @@
 // Put all your page JS here
 
 $(function () {
-    $('#slickQuiz').slickQuiz({
-        checkAnswerText:  'Check Answer',
-        nextQuestionText: 'Next <span class="glyphicon glyphicon-chevron-right"></span>',
-        preventUnansweredText: 'Your answer cannot be blank.',
-        randomSortQuestions: true,
-        randomSortAnswers: true,
-        preventUnanswered: true,
-        disableRanking: true,
-        scoreAsPercentage: true,
-        perQuestionResponseAnswers: true
-    });
+    $('#slickQuiz').slickQuiz();
 });
